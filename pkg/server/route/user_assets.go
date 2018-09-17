50ebac8db76129e4263e11a1ef91242af82008ee
Mark Sauter <dr.funk.mark@gmail.com>
2018-09-17 15:21:05 -0500

    add thumbnail creation to user_assets route

--- pkg/server/route/user_assets.go
+++ pkg/server/route/user_assets.go
@@ -3,6 +3,7 @@ package route
 import (
 	"io"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 	"github.com/marksauter/markus-ninja-api/pkg/myhttp"
@@ -10,15 +11,18 @@ import (
 	"github.com/marksauter/markus-ninja-api/pkg/mytype"
 	"github.com/marksauter/markus-ninja-api/pkg/repo"
 	"github.com/marksauter/markus-ninja-api/pkg/service"
+	minio "github.com/minio/minio-go"
 	"github.com/rs/cors"
 )
 
+// UserAssetsCors - cors options for user assets route
 var UserAssetsCors = cors.New(cors.Options{
 	AllowedHeaders: []string{"Content-Type"},
 	AllowedMethods: []string{http.MethodOptions, http.MethodGet},
 	AllowedOrigins: []string{"ma.rkus.ninja", "http://localhost:*"},
 })
 
+// UserAssetsHandler - handler for user assets route
 type UserAssetsHandler struct {
 	Repos      *repo.Repos
 	StorageSvc *service.StorageService
@@ -44,6 +48,25 @@ func (h UserAssetsHandler) ServeHTTP(rw http.ResponseWriter, req *http.Request) 
 
 	key := routeVars["key"]
 
+	s := req.URL.Query().Get("s")
+	var size int
+	if s != "" {
+		parsedSize, err := strconv.ParseInt(s, 10, 32)
+		if err != nil {
+			mylog.Log.WithError(err).Error("failed to parse size")
+			response := myhttp.InternalServerErrorResponse(err.Error())
+			myhttp.WriteResponseTo(rw, response)
+			return
+		}
+		size = int(parsedSize)
+		if size != 400 {
+			mylog.Log.WithError(err).Error("failed to parse size")
+			response := myhttp.InvalidRequestErrorResponse("size may only be 400")
+			myhttp.WriteResponseTo(rw, response)
+			return
+		}
+	}
+
 	assetPermit, err := h.Repos.Asset().GetByKey(req.Context(), key)
 	if err != nil {
 		mylog.Log.WithError(err).Error("failed to get asset")
@@ -60,16 +83,27 @@ func (h UserAssetsHandler) ServeHTTP(rw http.ResponseWriter, req *http.Request) 
 		return
 	}
 
-	asset, err := h.StorageSvc.Get(contentType, uid, key)
-	if err != nil {
-		mylog.Log.WithError(err).Error("failed to get file")
-		response := myhttp.InternalServerErrorResponse(err.Error())
-		myhttp.WriteResponseTo(rw, response)
-		return
+	var object *minio.Object
+	defer object.Close()
+	if s != "" {
+		object, err = h.StorageSvc.GetThumbnail(size, contentType, uid, key)
+		if err != nil {
+			mylog.Log.WithError(err).Error("failed to get thumbnail")
+			response := myhttp.InternalServerErrorResponse(err.Error())
+			myhttp.WriteResponseTo(rw, response)
+			return
+		}
+	} else {
+		object, err = h.StorageSvc.Get(contentType, uid, key)
+		if err != nil {
+			mylog.Log.WithError(err).Error("failed to get file")
+			response := myhttp.InternalServerErrorResponse(err.Error())
+			myhttp.WriteResponseTo(rw, response)
+			return
+		}
 	}
-	defer asset.Close()
 
-	n, err := io.Copy(rw, asset)
+	n, err := io.Copy(rw, object)
 	if err != nil {
 		mylog.Log.WithError(err).Error("failed to copy asset to response writer")
 		response := myhttp.InternalServerErrorResponse(err.Error())
